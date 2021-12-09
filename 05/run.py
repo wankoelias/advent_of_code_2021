dc07a9b4327c9c60cbc4d6dc9cd4449e961a6d46
Elias Wanko <elias.wanko@eox.at>
2021-12-09 09:38:11 +0100

    part2

--- 05/run.py
+++ 05/run.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+import numpy as np
 
 pointlist = list()
 
@@ -10,26 +11,54 @@ class Point:
     crosses = 0
 
 
+def print_arr(a):
+    a = a.astype(str)
+    a[a == "0"] = "."
+
+    for row in a.T:
+        print("".join(row))
+
+
 points = defaultdict(lambda: defaultdict(Point))
 
-
+# arr = np.zeros(shape=(10, 10), dtype=int)
 
 with open("input.txt") as f:
     data_in = f.read().split("\n")
     coords = [[eval(x) for x in d.split(" -> ")] for d in data_in]
 
 for (x_start, y_start), (x_end, y_end) in coords:
-    x_start, x_end = sorted((x_start, x_end))
-    y_start, y_end = sorted((y_start, y_end))
+    declining_x, declining_y = False, False
+
+    if x_start > x_end:
+        declining_x = True
+        x_start, x_end = x_end, x_start
+
+    if y_start > y_end:
+        declining_y = True
+        y_start, y_end = y_end, y_start
 
     if x_start == x_end:
         for y in range(y_start, y_end + 1):
             points[x_start][y].crosses += 1
+            # arr[x_start, y] += 1
 
-
-    if y_start == y_end:
+    elif y_start == y_end:
         for x in range(x_start, x_end + 1):
             points[x][y_start].crosses += 1
+            # arr[x, y_start] += 1
+
+    else:
+        for x, y in zip(range(x_start, x_end + 1), range(y_start, y_end + 1)):
+            if declining_x and not declining_y:
+                x = x_end - x + x_start
+            if declining_y and not declining_x:
+                y = y_end - y + y_start
+
+            points[x][y].crosses += 1
+            # arr[x, y] += 1
+        # print_arr(arr)
 
 
 print(len([x for x in pointlist if x.crosses > 1]))
+
