89fccd6653cacd1d9bda93266c312317c7c5955f
Elias Wanko <elias.wanko@eox.at>
2021-12-14 00:34:56 +0100

    day 9 part 2

--- 09/run.py
+++ 09/run.py
@@ -5,30 +5,54 @@ with open("input.txt") as f:
     data_in = f.read().split("\n")
 arr = np.array([[int(x) for x in d] for d in data_in])
 
-risk_level = 0
-
 ymax, xmax = arr.shape
 
-for y, x in product(*map(range, arr.shape)):
+visited_coords = set()
+pools = list()
 
-    def clip(c):
-        return 0 if c < 0 else c
 
-    n = arr[y, x]
+def coord(y, x):
+    return f"{y}-{x}"
+
+
+def get_adjacient(y, x):
 
     adjacient = list()
 
-    adjacient += [arr[y, x - 1]] if x > 0 else []
-    adjacient += [arr[y, x + 1]] if x < xmax - 1 else []
-    adjacient += [arr[y - 1, x]] if y > 0 else []
-    adjacient += [arr[y + 1, x]] if y < ymax - 1 else []
+    adjacient += [(y, x - 1)] if x > 0 else []
+    adjacient += [(y, x + 1)] if x < xmax - 1 else []
+    adjacient += [(y - 1, x)] if y > 0 else []
+    adjacient += [(y + 1, x)] if y < ymax - 1 else []
 
-    if min(adjacient) > n:
-        risk_level += 1 + n
+    return adjacient
 
-    # window = arr[clip(x - 1) : x + 2, clip(y - 1) : y + 2]
 
-    # if len(window[window <= n]) == np.product(window.shape) - 1:
-    #     risk_level += 1 + n
+def explore_pool(y, x, pool):
+    n = arr[y, x]
+    coordinate = coord(y, x)
 
-print(risk_level)
+    if n == 9:
+        return set()
+
+    if coordinate in visited_coords:
+        return set()
+
+    visited_coords.add(coordinate)
+
+    pool.add(coordinate)
+
+    for adj_y, adj_x in get_adjacient(y, x):
+        pool.update(explore_pool(adj_y, adj_x, pool))
+
+    return pool
+
+
+for py, px in product(*map(range, arr.shape)):
+
+    explored_pool = explore_pool(py, px, pool=set())
+
+    if len(explored_pool) > 0:
+        pools.append(explored_pool)
+
+
+print(np.product(sorted([len(x) for x in pools])[-3:]))
