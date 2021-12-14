65ade02aabf776fed1128c034a26f45fda175ba2
Elias Wanko <elias.wanko@eox.at>
2021-12-14 17:35:54 +0100

    day 10 part 2

--- 10/run.py
+++ 10/run.py
@@ -1,3 +1,5 @@
+from functools import reduce
+
 with open("input.txt") as f:
     data_in = f.read().split("\n")
 
@@ -8,28 +10,34 @@ mapping = {
     "[": "]",
 }
 
-scores = {
-    "}": 1197,
-    ")": 3,
-    ">": 25137,
-    "]": 57,
+scores_closing = {
+    ")": 1,
+    "]": 2,
+    "}": 3,
+    ">": 4,
 }
 
-score = 0
+score_list = list()
 
 for row in data_in:
     expected = list()
-    for i, char in enumerate(row):
+    broken = False
+    for char in row:
 
         if char in mapping.keys():
             expected.append(mapping[char])
         elif len(expected) == 0:
-            score += scores[char]
+            broken = True
             break
         elif char == expected[-1]:
             expected.pop()
         else:
-            score += scores[char]
+            broken = True
             break
 
-print(score)
+    if not broken:
+        score_list.append(
+            reduce(lambda a, b: a * 5 + b, [scores_closing[x] for x in expected[::-1]])
+        )
+
+print(sorted(score_list)[int(len(score_list) / 2)])
