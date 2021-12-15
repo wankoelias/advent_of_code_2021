9f373a25fbfa9d7ee9532927d173010a70cd1833
Elias Wanko <elias.wanko@eox.at>
2021-12-15 18:25:29 +0100

    simplified

--- 12/run.py
+++ 12/run.py
@@ -1,36 +1,35 @@
 from collections import defaultdict
 
-with open("input.txt") as f:
-    data_in = f.read().split("\n")
-
 cave = defaultdict(lambda: set())
 
-for line in data_in:
-    a, b = line.split("-")
-    cave[a].add(b)
-    cave[b].add(a)
+with open("input.txt") as f:
 
-all_paths = list()
+    for line in f:
+        a, b = line.rstrip().split("-")
+        cave[a].add(b)
+        cave[b].add(a)
+
+n_paths = 0
 
 
 def find_paths(node, path=list()):
 
     if node == "end":
-        path.append(node)
-        all_paths.append(path)
+        global n_paths
+        n_paths += 1
         return
 
     blocked = {x for x in set(path) if x.islower()}
 
-    blocked_small_caves = blocked - {"start", "end"}
+    blocked_small_caves = blocked - {"start"}
 
     path.append(node)
 
-    double_visits = len({x for x in path if x.islower() and path.count(x) > 1})
+    small_cave_revisits = len({x for x in blocked_small_caves if path.count(x) > 1})
 
-    small_revisit = blocked_small_caves if double_visits == 0 else set()
+    small_caves_to_revisit = blocked_small_caves if small_cave_revisits == 0 else set()
 
-    possible_next = (cave[node] - blocked).union(small_revisit & cave[node])
+    possible_next = (cave[node] - blocked).union(small_caves_to_revisit & cave[node])
 
     for next_node in possible_next:
         find_paths(next_node, path.copy())
@@ -38,4 +37,4 @@ def find_paths(node, path=list()):
 
 find_paths("start")
 
-print(len(all_paths))
+print(n_paths)
