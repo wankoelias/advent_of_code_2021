566f8ebf44772e8dfca7bb37990c6bf7b7d85c09
Elias Wanko <elias.wanko@eox.at>
2021-12-19 18:58:18 +0100

    day 13 part 1

---
+++ 13/run.py
@@ -0,0 +1,59 @@
+from collections import defaultdict
+import re
+
+with open("input.txt") as f:
+    dot_input, fold_input = f.read().split("\n\n")
+
+dots = defaultdict(lambda: defaultdict(lambda: None))
+iterdots = lambda: (dots[x][y] for x in dots for y in dots[x] if dots[x][y])
+
+
+class Dot:
+    def __init__(self, x, y):
+        dots[x][y] = self
+        self._x = x
+        self._y = y
+
+    @property
+    def x(self):
+        return self._x
+
+    @property
+    def y(self):
+        return self._y
+
+    def setxy(self, attr, value):
+        dots[self._x][self._y] = None
+        setattr(self, attr, value)
+        dots[self._x][self._y] = self
+
+    @x.setter
+    def x(self, value):
+        self.setxy("_X", value)
+
+    @y.setter
+    def y(self, value):
+        self.setxy("_y", value)
+
+    def fold(self, axis, fold_pos):
+        if getattr(self, axis) > fold_pos:
+
+            self.setxy("_" + axis, fold_pos - (getattr(self, axis) - fold_pos))
+
+    def __repr__(self):
+        return f"Dot({self._x}, {self._y})"
+
+
+for p in dot_input.split("\n"):
+    Dot(*eval(p))
+
+
+for f in fold_input.split("\n"):
+    axis, pos = re.match("fold along ([x,y])=(\d+)", f).groups()
+    pos = int(pos)
+
+    for p in list(iterdots()):
+        p.fold(axis, pos)
+    break
+
+print(len(list(iterdots())))
