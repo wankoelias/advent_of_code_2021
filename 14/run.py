a4b04f86941f048a67bf8d0fdcae4a8bdb2c13f8
Elias Wanko <elias.wanko@eox.at>
2021-12-23 22:51:10 +0100

    day 14 part 2

--- 14/run.py
+++ 14/run.py
@@ -7,27 +7,37 @@ rules = defaultdict(
     str, {k: v for k, v in map(lambda r: r.split(" -> "), rules_input.split("\n"))}
 )
 
-max_iteration = 10
+max_iteration = 40
+
+polymerized_pairs = dict()
 
 
-def polymerize_pair(a, b, iteration=0, max_iteration=0, count=Counter()):
+def polymerize_pair(a, b, iteration=0, max_iteration=0):
+    count = Counter()
+    pair = (a, b, iteration)
+
+    if pair in polymerized_pairs:
+        return polymerized_pairs[pair]
 
     if iteration == max_iteration:
         return count
 
-    insertion = rules["{a}{b}".format(a=a, b=b)]
+    insertion = rules[a + b]
 
     if insertion:
         count[insertion] += 1
 
-        count = polymerize_pair(a, insertion, iteration + 1, max_iteration, count)
-        count = polymerize_pair(insertion, b, iteration + 1, max_iteration, count)
+        count += polymerize_pair(a, insertion, iteration + 1, max_iteration)
+        count += polymerize_pair(insertion, b, iteration + 1, max_iteration)
+
+    polymerized_pairs[pair] = count
+
     return count
 
 
 counter = Counter(polymer)
 
 for a, b in pairwise(polymer):
-    counter = polymerize_pair(a, b, max_iteration=max_iteration, count=counter)
+    counter += polymerize_pair(a, b, max_iteration=max_iteration)
 
 print(max(counter.values()) - min(counter.values()))
